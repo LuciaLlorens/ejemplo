f71eccac839f0390b25f269b34824d6c8f74e4cb
LuciaLlorens <luciallrns@gmail.com>
2023-06-13 13:03:43 -0300

    Agrego el archivo miNombreCompleto.js

---
+++ practico1/miNombreCompleto.js
@@ -0,0 +1,9 @@
+let nombre;
+nombre = "Lucía";
+let apellido = "Llorens";
+let edad = 23;
+
+console.log("Hola, %s %s. Tienes %d años.", nombre, apellido, edad);
+
+// Imprime por consola:
+// Hola, Lucía Llorens. Tienes 23 años.
